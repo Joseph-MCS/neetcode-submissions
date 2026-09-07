3fdd8328a5c96087972a53d3965e7936d6ee5dc3
Joseph McSweeney <163135970+Joseph-MCS@users.noreply.github.com>
2026-09-07 19:18:44 +0100

    Add: design-browser-history - submission-0

---
+++ Data Structures & Algorithms/design-browser-history/submission-0.java
@@ -0,0 +1,62 @@
+public class ListNode{
+    String val; 
+    ListNode next, prev; 
+
+    ListNode( String val )
+    {
+        this.val = val; 
+        this.next = null;
+        this.prev = null;
+    }
+
+    ListNode( String val, ListNode prev, ListNode next )
+    {
+        this.val = val; 
+        this.next = next; 
+        this.prev = prev; 
+    }
+}
+
+class BrowserHistory {
+
+    ListNode curr; 
+
+    public BrowserHistory(String homepage) 
+    {
+        curr = new ListNode(homepage, null, null);
+    }
+    
+    public void visit(String url) {
+
+        ListNode page = new ListNode( url, curr, null );
+        curr.next = page; 
+        curr = page;
+        
+    }
+    
+    public String back(int steps) {
+        while ( steps > 0 && curr.prev != null )
+        {
+            curr = curr.prev;
+            steps--;
+        }
+        return curr.val;
+    }
+    
+    public String forward(int steps) {
+        while ( steps > 0 && curr.next != null )
+        {
+            curr = curr.next;
+            steps--;
+        }
+        return curr.val;
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
