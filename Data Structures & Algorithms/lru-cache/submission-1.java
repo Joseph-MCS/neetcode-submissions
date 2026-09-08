85139f44a87c706e061bf43738e259ca37c8ee2d
Joseph McSweeney <163135970+Joseph-MCS@users.noreply.github.com>
2026-09-08 23:15:05 +0100

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.java
@@ -0,0 +1,85 @@
+public class ListNode{
+    int key, val; 
+    ListNode next,prev; 
+
+    public ListNode(int key, int val)
+    {
+        this.key = key;
+        this.val = val; 
+    }
+}
+
+class LRUCache {
+
+    ListNode head; // the least recently used 
+    ListNode tail; // the most recently used
+    HashMap<Integer, ListNode> cache; 
+    int capacity; 
+
+    public LRUCache(int capacity) {
+
+        this.head = new ListNode(0,0);
+        this.tail = new ListNode(0,0);
+        this.capacity = capacity; 
+        this.cache = new HashMap<>();
+        // dummy sentinel nodes for the most recently used ( tail ) and least recently used ( head )
+        this.head.next = this.tail; 
+        this.tail.prev = this.head;
+        
+    }
+
+    private void remove(ListNode node)
+    {
+        ListNode pred = node.prev;
+        ListNode succ = node.next; 
+        pred.next = succ; 
+        succ.prev = pred;
+
+    }
+
+    private void insert(ListNode node)
+    {    
+        ListNode newNode = node; 
+        newNode.prev = this.tail.prev; 
+        newNode.next = this.tail; 
+        this.tail.prev.next = newNode; 
+        this.tail.prev = newNode;
+    }
+    
+    public int get(int key) 
+    {
+        if ( (cache.containsKey(key)) ) 
+        {
+            ListNode node = cache.get(key); 
+            remove(node);
+            insert(node);
+            return node.val;
+        }
+        else return -1; 
+    }
+    
+    public void put(int key, int value) 
+    {
+        if ( cache.containsKey(key) )
+        {
+            // update existing key 
+            ListNode existingNode = cache.get(key);
+            remove(existingNode);
+            existingNode.val = value; 
+            insert(existingNode);
+            return;
+        }
+
+        ListNode newNode = new ListNode(key,value);
+        cache.put(key,newNode);
+        insert(newNode);
+
+        if ( cache.size() > capacity )
+        {
+           ListNode leastRecentNode = this.head.next; 
+           remove(leastRecentNode);
+           cache.remove(leastRecentNode.key);
+        }
+        
+    }
+}
